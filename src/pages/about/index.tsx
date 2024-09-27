c4f478487d74dacb142ff54fe2efaa9f3ff5cc8c
Samuil Prytchyn <samuil-18@tut.by>
2024-09-28 01:09:14 +0200

    comment Services and Reviews in AboutMe page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,12 @@
 import classes from './about.module.css';
 import { AboutMe } from './about-me';
-import { Services } from './services';
-import { Reviews } from './reviews';
 
 const About = () => {
 	return (
 		<section className={classes.wrapper}>
 			<AboutMe/>
-			<Services/>
-			<Reviews/>
+			{/*<Services/>*/}
+			{/*<Reviews/>*/}
 		</section>
 	);
 };
